c3d7c709aab97f55cba02523f66c737561ba6db0
Jaeha Lee <wogkdkrm112@gmail.com>
2023-01-14 00:25:14 +0900

    ✒️Feat: 로또 - 45개의 공 무작위뽑기

---
+++ lotto/main.js
@@ -0,0 +1,31 @@
+const candidate = Array(45)
+  .fill()
+  .map((v, i) => {
+    return i + 1;
+  });
+const shuffle = [];
+
+while (candidate.length > 0) {
+  // 무작위 인덱스 뽑기
+  const random = Math.floor(Math.random() * candidate.length);
+
+  // 뽑은 값은 배열에 들어 있다
+  const spliceArray = candidate.splice(random, 1);
+
+  // 배열에 들어 있는 값을 꺼내서
+  const value = spliceArray[0];
+
+  // shuffle 배열에 넣기
+  shuffle.push(value);
+}
+console.log(shuffle);
+
+/** while -> for문
+for (let i = candidate.length; i > 0; i--) {
+  const random = Math.floor(Math.random() * i);
+  const spliceArray = candidate.splice(random, 1);
+  const value = spliceArray[0];
+  shuffle.push(value);
+}
+console.log(shuffle);
+ */
