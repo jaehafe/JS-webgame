5e3046f2bd54f858fca097d4a63911b5e15acf80
Jaeha Lee <wogkdkrm112@gmail.com>
2023-01-16 00:11:58 +0900

    🛠Fix: 가위 바위 보 버튼을 여러번 눌렀을 때 발생하는 버그 수정
    1. clickable 플래그 변수로 수정
    2. removeEventListener로도 수정 가능

--- rock-paper-sicssor/main.js
+++ rock-paper-sicssor/main.js
@@ -8,14 +8,14 @@ $computer.style.background = `url(${IMG_URL}) 0 0`;
 $computer.style.backgroundSize = 'auto 200px';
 
 const rspX = {
-  scissor: '0',
+  scissors: '0',
   rock: '-220px',
   paper: '-440px',
 };
 
 let computerChoice = 'scissors';
 const changeComputerHand = () => {
-  if (computerChoice === 'scissor') {
+  if (computerChoice === 'scissors') {
     // 가위면
     computerChoice = 'rock';
   } else if (computerChoice === 'rock') {
@@ -23,9 +23,44 @@ const changeComputerHand = () => {
     computerChoice = 'paper';
   } else if (computerChoice === 'paper') {
     // 보
-    computerChoice = 'scissor';
+    computerChoice = 'scissors';
   }
   $computer.style.background = `url(${IMG_URL}) ${rspX[computerChoice]} 0`;
   $computer.style.backgroundSize = 'auto 200px';
+  console.log('rock');
 };
-setInterval(changeComputerHand, 50);
+
+let intervalId = setInterval(changeComputerHand, 50);
+
+// 버그수정 첫번째 방법
+// const clickButton = () => {
+//   clearInterval(intervalId);
+//   $rock.removeEventListener('click', clickButton);
+//   $scissors.removeEventListener('click', clickButton);
+//   $paper.removeEventListener('click', clickButton);
+
+//   setTimeout(() => {
+//     intervalId = setInterval(changeComputerHand, 50);
+//     $rock.addEventListener('click', clickButton);
+//     $scissors.addEventListener('click', clickButton);
+//     $paper.addEventListener('click', clickButton);
+//   }, 1000);
+// };
+
+// 버그수정 두번째 방법
+let clickable = true;
+const clickButton = () => {
+  if (clickable) {
+    clearInterval(intervalId);
+    clickable = false;
+
+    setTimeout(() => {
+      clickable = true;
+      intervalId = setInterval(changeComputerHand, 50);
+    }, 1000);
+  }
+};
+
+$rock.addEventListener('click', clickButton);
+$scissors.addEventListener('click', clickButton);
+$paper.addEventListener('click', clickButton);
